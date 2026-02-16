5fc29ea1a48fa5d159a53a1edf2804473214ea9b
inuzuka-LaTrace <inuzuka8080@gmail.com>
2026-02-16 19:04:18 +0900

    Create posts.config.js

---
+++ posts.config.js
@@ -0,0 +1,17 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+### **ファイル6: .gitignore**
+
+**ファイル名**: `.gitignore`
+```
+node_modules
+dist
+dist-ssr
+*.local
+.DS_Store
